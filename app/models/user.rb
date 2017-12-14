cad5c8485fdf1fbff21ba13dfc51217a2dee17ad
Rossnaive <ross.lee8888@gmail.com>
2017-12-14 06:55:05 +0000

    deploy first

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  has_many :products, through: :orders
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :timeoutable, :lockable
+  #has_many :products, through: :orders
 end
