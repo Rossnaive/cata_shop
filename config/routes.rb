9fc913b0e20026dd5da656388ecdddf1cbdfcf5a
Rossnaive <ross.lee8888@gmail.com>
2017-12-14 10:14:48 +0000

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile', :confirmation => 'confirm/:confirmation_token'}, :controllers => {:omniauth_callbacks => 'omniauth_callbacks'}
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'}
   resources :users
   resources :products
   resources :categories
